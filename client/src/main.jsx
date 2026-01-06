e529d06a8a91cef83a30d878aa2bcc465eedcf3d
Purvesh_PJ <Purvesh9997@gmail.com>
2026-01-06 16:39:51 +0530

    fix: remove Radix Theme wrapper from main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { Theme } from '@radix-ui/themes';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import { ErrorBoundary } from './components/common/ErrorBoundary';
 import { ThemeProvider } from './styles/ThemeProvider';
-import '@radix-ui/themes/styles.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,10 +23,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <ThemeProvider>
-            <Theme accentColor="gray" grayColor="slate" radius="small" scaling="100%">
-              <App />
-              <Toaster position="top-right" />
-            </Theme>
+            <App />
+            <Toaster position="top-right" />
           </ThemeProvider>
         </BrowserRouter>
       </QueryClientProvider>
