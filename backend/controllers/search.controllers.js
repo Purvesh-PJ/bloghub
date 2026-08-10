536cd4ca0c09fdfc610fdbbb13c4f086c16daf23
purvesh-pj <Purvesh9997@gmail.com>
2026-08-11 01:46:11 +0530

    fix(search): sanitize regex input to prevent ReDoS attacks

--- backend/controllers/search.controllers.js
+++ backend/controllers/search.controllers.js
@@ -4,12 +4,17 @@ const cheerio = require('cheerio');
 
 exports.getSearchQueries = async (req, res) => {
   const { query } = req.params;
-  console.log('backend response :', query);
+
+  if (!query || typeof query !== 'string') {
+    return res.status(400).json({ success: false, message: 'Search query required' });
+  }
 
   try {
+    const sanitizedQuery = query.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+
     const results = await Post.aggregate([
       {
-        $match: { title: { $regex: query, $options: 'i' } },
+        $match: { title: { $regex: sanitizedQuery, $options: 'i' } },
       },
       {
         $project: { title: 1, truncatedContent: { $substr: ['$content', 0, 200] } },
@@ -17,20 +22,15 @@ exports.getSearchQueries = async (req, res) => {
     ]);
 
     results.forEach((element) => {
-      const $ = cheerio.load(element.truncatedContent);
+      const $ = cheerio.load(element.truncatedContent || '');
       element.truncatedContent = $.text() + '...';
-      // if(element.truncatedContent.length < element.content.length){
-      //     element.truncatedContent += '...';
-      // }
     });
 
-    res.status(201).json({
+    return res.status(200).json({
       success: true,
       message: 'Search succesfully',
       data: results,
     });
-
-    console.log(results);
   } catch (error) {
     res.status(500).json({
       success: false,
